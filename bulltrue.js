4fcf2fb2c8f04dc29d08820ca00bbf7157d977b8
Chris1114 <53130727+Chris1114@users.noreply.github.com>
2021-03-04 22:46:36 -0500

    Add files via upload

--- bulltrue.js
+++ bulltrue.js
@@ -43,7 +43,7 @@ var clickedBull = 0;
 var cyn = 0;//cynical-bullWrong counter
 var gul = 0;//gullible-trueWrong counter
 var playerAnswer;
-var score = 961;
+var score = 1025;
 var wager25;
 var wager50;
 var wager75;
